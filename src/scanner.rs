3ae60e55f0032e02b4388887f9062d8eccbc9f81
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-08-23 16:11:32 -0300

    chore: remove comments

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,19 +3,16 @@ use std::time::Duration;
 
 /// Scan ports from 1 to 1024
 pub fn scan_host(host: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Resolve o host uma vez só
-    let ips: Vec<IpAddr> = (host, 0) // porta dummy
-        .to_socket_addrs()? // resolve host
+    let ips: Vec<IpAddr> = (host, 0)
+        .to_socket_addrs()?
         .map(|addr| addr.ip())
         .collect();
 
-    // Pré-monta todos os SocketAddr (ip + porta)
     let socket_addrs: Vec<SocketAddr> = ips
         .iter()
         .flat_map(|ip| (1..=1024).map(move |port| SocketAddr::new(*ip, port)))
         .collect();
 
-    // Agora só testa cada socket
     for addr in socket_addrs {
         if TcpStream::connect_timeout(&addr, Duration::from_millis(200)).is_ok() {
             println!("Port {} open on {}", addr.port(), addr.ip());
